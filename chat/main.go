1c1a12c720d4b1ea049bd68b37a7371be103c0e8
sommano phivilay <sommano@gmail.com>
2018-08-02 00:14:14 -0400

    ch1

--- chat/main.go
+++ chat/main.go
@@ -1,12 +1,32 @@
-package main 
-import ( 
-  "log" 
-  "net/http" 
-) 
+package main
 
-func main() { 
-  http.HandleFunc("/", func(w http.ResponseWriter, r  *http.Request) { 
-    w.Write([]byte(`
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
       <html> 
         <head> 
           <title>Chat</title> 
@@ -14,15 +34,12 @@ func main() {
         <body> 
           Let's chat! 
         </body> 
-      </html> 
-    )) 
-  }) 
+      </html>
+    `))
+	})
 
-  // start the web server 
-  if err := http.ListenAndServe(":8080", nil); 
-err != nil { 
-    log.Fatal("ListenAndServe:", err) 
-  } 
-} 
-
-
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
