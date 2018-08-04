0716d9bd840fdf06cc22fedf2a4922b8c751a247
sommano phivilay <sommano@gmail.com>
2018-08-04 01:11:22 -0400

    ch1

--- chat/client.go
+++ chat/client.go
@@ -13,3 +13,23 @@ type client struct {
 	// room is the room this client is chatting in.
 	room *room
 }
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
