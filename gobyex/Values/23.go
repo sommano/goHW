eb958335e183c3ff54b99900c081db1097f82014
Sommano Phivilay <sommano@gmail.com>
2018-11-12 12:07:33 -0600

    Create 23.go

---
+++ gobyex/Values/23.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// The `(int, int)` in this function signature shows that
+// the function returns 2 `int`s.
+func vals() (int, int) {
+    return 3, 7
+}
+
+func main() {
+
+    // Here we use the 2 different return values from the
+    // call with _multiple assignment_.
+    a, b := vals()
+    fmt.Println(a)
+    fmt.Println(b)
