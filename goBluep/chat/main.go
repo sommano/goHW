03e07298947cc08a645795e482a9275ad75f3c7f
sommano <sommano@gmail.com>
2018-11-22 19:40:50 -0600

    ch1

--- goBluep/chat/main.go
+++ goBluep/chat/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"sync"
 )
+
+// templ represents a single template
+type templateHandle struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
