dc98fa68bec14a455876226635135613790c88cc
Sommano Phivilay <sommano@gmail.com>
2018-11-26 15:34:33 -0600

    Create 34.go

---
+++ gobyex/Values/34.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+
+    // `defer`s will _not_ be run when using `os.Exit`, so
+    // this `fmt.Println` will never be called.
+    defer fmt.Println("!")
+
+    // Exit with status 3.
+    os.Exit(3)
+}
